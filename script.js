04ca2e8dc9ab82e86eb8a86ad06b2afbf82b302c
Diana Cholankeril <dianac@id.iit.edu>
2017-04-13 14:40:01 -0500

    updates

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 
 // for (var i = 0; i<=4; i++) {
 //   boxes[i].addEventListener("click", boxClicked_ + i);
+
+
+function boxClicked_0() {
+  console.log("Hey, 1 box clicked!")
+
+  leftMargin[0] += moveAmount;
+
+  boxes[0].style.marginLeft = leftMargin[0] + "px";
+
+}
+
+var count = 0;
+document.getElementById('button').onclick = function() {
+   alert("button was clicked " + (count++) + " times");
+};
+
+
+boxes[0].addEventListener("click", boxClicked_0);
