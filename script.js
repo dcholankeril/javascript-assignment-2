764ca850b7691f56e6020576950549d3266d4707
Diana Cholankeril <dianac@id.iit.edu>
2017-04-13 18:41:12 -0500

    more stuff

--- script.js
+++ script.js
@@ -3,20 +3,6 @@
 // for (var i = 0; i<=4; i++) {
 //   boxes[i].addEventListener("click", boxClicked_ + i);
 
-
-function boxClicked_0() {
-  console.log("Hey, 1 box clicked!")
-
-  leftMargin[0] += moveAmount;
-
-  boxes[0].style.marginLeft = leftMargin[0] + "px";
-
-}
-
-var count = 0;
-document.getElementById('button').onclick = function() {
-   alert("button was clicked " + (count++) + " times");
-};
-
-
-boxes[0].addEventListener("click", boxClicked_0);
+var boxes = document.getElementsByClassName('box');
+var numBoxes = boxes.length;
+var color = 'blue'
