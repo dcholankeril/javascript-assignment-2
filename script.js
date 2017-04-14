2798ddf4b3937824d7766fdee555cd134edece75
Diana Cholankeril <dianac@id.iit.edu>
2017-04-13 19:03:11 -0500

    creating a variable called color. later we use color, by making a switch and case statement (if, else statements). so depending on the number of red boxes that are on the page, these switch and case statements say what color to turn the boxes.

--- script.js
+++ script.js
@@ -6,3 +6,40 @@
 var boxes = document.getElementsByClassName('box');
 var numBoxes = boxes.length;
 var color = 'blue'
+
+switch (numBoxes / 100 | 0) {
+    case 2:
+        color = 'yellow'
+        break;
+    case 3:
+        color = 'green'
+        break;
+    case 4:
+        color = 'purple'
+        break;
+    case 5:
+        color = 'orange'
+        break;
+    case 6:
+        color = 'pink'
+        break;
+    case 7:
+        color = 'black'
+        break;
+    case 8:
+        color = 'brown'
+        break;
+    default:
+        color = 'blue'
+}
+
+
+clickHandler = function() {
+    this.style.backgroundColor = color;
+    this.classList.toggle('clicked');
+}
+
+for (var i = 0; i < numBoxes; i++) {
+    var box = boxes[i];
+    box.addEventListener('click', clickHandler);
+}
